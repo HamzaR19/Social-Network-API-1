ea9cf9e9da151b5152a43ba85dd64df40ee561c2
HamzaR19 <132932060+HamzaR19@users.noreply.github.com>
2023-08-06 22:59:29 -0500

    trying to finish file structure

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+const {connect, connection} = require('mongoose');
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/social_network_db');
+connect('mongodb://127.0.0.1:27017/social_network_db');
 
-module.exports = mongoose.connection;
+module.exports = connection;
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const api = require('./api');
 
 
-router.use('/api', api);
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-    return res.send('WRONG ROUTE!');
+    return res.send('WRONG ROUTE!!@');
   });
 
 module.exports = router;
